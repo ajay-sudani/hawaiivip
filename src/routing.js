c4a9c5878ec174cc63d77ba3f664aa7410bc24ec
Ajay Sudani <ajaysudani07@gmail.com>
2019-01-07 11:33:19 +0530

    added homepgae

--- src/routing.js
+++ src/routing.js
@@ -3,16 +3,17 @@ import Aux from './hoc/Aux';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom'
 import Home from './components/home/home';
 import Dashboard from './components/dashboard/dashboard';
+const url = process.env.NODE_ENV === 'production' ? '/hawaiivip' : '';
 
 class AppRouting extends Component {
     render() {
         return (
             <BrowserRouter>
                 <Aux>
-                    <Route path="/home" component={Home} />
-                    <Route path="/dashboard" component={Dashboard} />
-                    <Route exact path="/" render={() => (
-                        <Redirect to="/home" />
+                    <Route path={url + '/home'} component={Home} />
+                    <Route path={url + '/dashboard'} component={Dashboard} />
+                    <Route exact path={url + '/'} render={() => (
+                        <Redirect to={url + '/home'} />
                     )} />
                 </Aux>
             </BrowserRouter>
