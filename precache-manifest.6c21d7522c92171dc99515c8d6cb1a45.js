1dcfbf8f993e358ea908d21ca5647fb273be43c3
Ajay Sudani <ajaysudani07@gmail.com>
2019-01-07 11:58:59 +0530

    Updates

---
+++ precache-manifest.6c21d7522c92171dc99515c8d6cb1a45.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dfb487fa4f6efa66e2d281e7d8e7d48",
+    "url": "/hawaiivip/static/media/activity_2.1dfb487f.png"
+  },
+  {
+    "revision": "1582351b48368f03496c",
+    "url": "/hawaiivip/static/css/main.dab81222.chunk.css"
+  },
+  {
+    "revision": "6e3bb5e6fefae0d628e0",
+    "url": "/hawaiivip/static/js/1.6e3bb5e6.chunk.js"
+  },
+  {
+    "revision": "aebe73cdd14edaae1a8b",
+    "url": "/hawaiivip/static/js/runtime~main.aebe73cd.js"
+  },
+  {
+    "revision": "845c83264c5321f32114b5debcefd9ed",
+    "url": "/hawaiivip/static/media/accommodation_1.845c8326.png"
+  },
+  {
+    "revision": "65928ed86652509d0c6edee880cd7b2d",
+    "url": "/hawaiivip/static/media/accommodation_2.65928ed8.png"
+  },
+  {
+    "revision": "f504db104d23822f7f1ae676fce9bd9d",
+    "url": "/hawaiivip/static/media/accommodation_3.f504db10.png"
+  },
+  {
+    "revision": "01471aca39420f019cef66daf6dc986c",
+    "url": "/hawaiivip/static/media/accommodation_4.01471aca.png"
+  },
+  {
+    "revision": "7dda575eaee8638fd85608e0c1bd0a67",
+    "url": "/hawaiivip/static/media/accommodation_5.7dda575e.png"
+  },
+  {
+    "revision": "8e1b4f16cc893d2ff638ab9a35848a48",
+    "url": "/hawaiivip/static/media/accommodation_6.8e1b4f16.png"
+  },
+  {
+    "revision": "845c83264c5321f32114b5debcefd9ed",
+    "url": "/hawaiivip/static/media/accommodation_3.845c8326.png"
+  },
+  {
+    "revision": "f504db104d23822f7f1ae676fce9bd9d",
+    "url": "/hawaiivip/static/media/accommodation_2.f504db10.png"
+  },
+  {
+    "revision": "65928ed86652509d0c6edee880cd7b2d",
+    "url": "/hawaiivip/static/media/accommodation_1.65928ed8.png"
+  },
+  {
+    "revision": "1582351b48368f03496c",
+    "url": "/hawaiivip/static/js/main.1582351b.chunk.js"
+  },
+  {
+    "revision": "a2f73847dea8958db93610ce5f930a64",
+    "url": "/hawaiivip/static/media/vip_3.a2f73847.png"
+  },
+  {
+    "revision": "633533e3d97e32c48bd1c2bf911da7a3",
+    "url": "/hawaiivip/static/media/private_chefs.633533e3.png"
+  },
+  {
+    "revision": "fb37d5758d8363145077869c86264fa8",
+    "url": "/hawaiivip/static/media/activity_1.fb37d575.png"
+  },
+  {
+    "revision": "edaafb3ce47c6a410c016b508ce2603f",
+    "url": "/hawaiivip/static/media/activity_3.edaafb3c.png"
+  },
+  {
+    "revision": "0b8d16cabf87fb2841f213ddd83dc7a7",
+    "url": "/hawaiivip/static/media/vip_1.0b8d16ca.png"
+  },
+  {
+    "revision": "f48dbd16c3deb28642629298ac37e9b6",
+    "url": "/hawaiivip/static/media/vip_2.f48dbd16.png"
+  },
+  {
+    "revision": "61ea2752bb3701a98584278fd0f26393",
+    "url": "/hawaiivip/static/media/vip_4.61ea2752.png"
+  },
+  {
+    "revision": "a50c5b51cb65fc6dc60ea8dbab71c49f",
+    "url": "/hawaiivip/static/media/background_1.a50c5b51.png"
+  },
+  {
+    "revision": "04db8cf0c52ca89818bd1de606ad01ba",
+    "url": "/hawaiivip/static/media/sunshine_makena_fairway_landing.04db8cf0.png"
+  },
+  {
+    "revision": "4a8f20674cb902ccedd3212eb751b5a9",
+    "url": "/hawaiivip/static/media/bg_cover_1.4a8f2067.png"
+  },
+  {
+    "revision": "e598d165d10f874bfa7d1f581da1ce80",
+    "url": "/hawaiivip/static/media/napali_coast_1.e598d165.png"
+  },
+  {
+    "revision": "47c8de82807ec16e4ad51e06df84cdc4",
+    "url": "/hawaiivip/static/media/napali_coast_2.47c8de82.png"
+  },
+  {
+    "revision": "ae75ccf478e8347dbe763fec23c62966",
+    "url": "/hawaiivip/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9d1b4a89d8cd9c1afce4ea9ca38712e4.js"
+  "/hawaiivip/precache-manifest.6c21d7522c92171dc99515c8d6cb1a45.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/hawaiivip/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
