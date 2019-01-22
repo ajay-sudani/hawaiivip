f7e9fcb8b7f8693469f92bcf5951f39b9c08078b
Ajay Sudani <ajaysudani07@gmail.com>
2019-01-22 11:38:50 +0530

    given changes

--- src/routing.js
+++ src/routing.js
@@ -3,8 +3,6 @@ import Aux from './hoc/Aux';
 import { Route, Redirect, HashRouter } from 'react-router-dom'
 import Home from './components/home/home';
 import Dashboard from './components/dashboard/dashboard';
-import AboutUs from './components/about-us/about-us';
-import ContactUs from './components/contact-us/contact-us';
 
 const url = process.env.NODE_ENV === 'production' ? '' : '';
 
@@ -15,8 +13,6 @@ class AppRouting extends Component {
                 <Aux>
                     <Route path={url + '/home'} component={Home} />
                     <Route path={url + '/dashboard'} component={Dashboard} />
-                    <Route path={url + '/about-us'} component={AboutUs} />
-                    <Route path={url + '/contact-us'} component={ContactUs} />
                     <Route exact path={url + '/'} render={() => (
                         <Redirect to={url + '/home'} />
                     )} />
